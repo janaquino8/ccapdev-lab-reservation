c387c937edd6fe76b9c126e93537c6bc67d04634
Childax <richmondramos117@gmail.com>
2026-02-08 16:23:14 +0800

    added tailwind css and initial navbar

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,24 @@
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  const navLinks = [
+    { name: 'Create Reservation', href: '#' },
+    { name: 'Edit Reservation', href: '#' },
+    { name: 'Previous Reservations', href: '#' },
+    { name: 'View Slots', href: '#' },
+    { name: 'Search', href: '#' },
+  ];
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.linkContainer}>
+        {navLinks.map((link) => (
+          <a key={link.name} href={link.href} className={styles.navLink}>{link.name}</a>
+        ))}
+        <div className={styles.profileCircle}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
