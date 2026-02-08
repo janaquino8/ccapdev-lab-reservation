0bd92faaa847853213ae1808f26b97ad50ca8a4a
janaquino8 <janaquino53@gmail.com>
2026-02-08 23:42:48 +0800

    refactor: separate css per component

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,32 +1,14 @@
 import './App.css'
 import '../components/Navbar/navbar.tsx'
 import Navbar from '../components/Navbar/navbar.tsx'
-import Slot from '../components/Slot/Slot.tsx'
-import Desk from '../components/Desk/Desk.tsx';
+import ViewSlots from '../pages/ViewSlotsPage.tsx'
 
 function App() {
 
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', padding: '40px' }}>
-        <Desk 
-          topSlots={[
-            { id: 'A1', status: 'available' },
-            { id: 'A2', status: 'reserved' }
-          ]} 
-          
-        />
-
-        <Desk 
-          bottomSlots={[
-            { id: 'A7', status: 'unavailable' },
-            { id: 'A8', status: 'available' }
-          ]}
-        />
-      </div>
+      <Navbar />
+      <ViewSlots />
     </>
   )
 }
--- client/src/pages/ViewSlotsPage.tsx
+++ client/src/pages/ViewSlotsPage.tsx
@@ -1,6 +1,37 @@
+import Desk from '../components/Desk/Desk.tsx';
+
 const ViewSlots = () => {
+    
+    const deskStyle = {
+        display: 'flex' as const,
+        flexWrap: 'wrap' as const, 
+        gap: '20px',
+        padding: '40px',
+    }
+
     return (
         <>
+            <div style={deskStyle}>
+                <Desk 
+                topSlots={[
+                    { id: 'A1', status: 'available' },
+                    { id: 'A2', status: 'reserved' }
+                ]} 
+                />
+
+                <Desk 
+                bottomSlots={[
+                    { id: 'A7', status: 'unavailable' },
+                    { id: 'A8', status: 'available' }
+                ]}
+                />
+
+                <Desk 
+                bottomSlots={[
+                    { id: 'A7', status: 'unavailable' }
+                ]}
+                />
+            </div>
         </>
     );
 };
