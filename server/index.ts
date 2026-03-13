aa5b3431b76c55252ce3bbe71827fb9ed0a0e39b
janaquino8 <janaquino53@gmail.com>
2026-03-14 01:48:01 +0800

    fix: db connectin

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,7 @@ import laboratoryRouter from './routes/laboratory.routes';
 import reservationRouter from './routes/reservation.routes';
 import slotRouter from './routes/slot.routes';
 import userRouter from './routes/user.routes';
+import connect_db from './config/db.ts'
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,8 @@ const host = process.env.DB_HOST || 'localhost:';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connect_db();
 
 app.use('/auth', authRouter);
 app.use('/laboratories', laboratoryRouter);
