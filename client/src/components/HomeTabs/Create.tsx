cab17f8cada86a378d941ffc8c865039e8da27cb
janaquino8 <janaquino53@gmail.com>
2026-03-21 15:35:51 +0800

    fix: create reservation linking in home

--- client/src/components/HomeTabs/Create.tsx
+++ client/src/components/HomeTabs/Create.tsx
@@ -19,9 +19,9 @@ const Create = () => {
             <strong>DLSU Comp. Labs</strong> have three laboratories with <strong>32 slots</strong> that students can reserve in advance. Click on <strong>“Reserve a Slot”</strong> on the desired computer laboratory.
         </span>
       </p>
-      <RoomBar roomName="Gokongwei Room 307A" onReserve={() => handleReserve("307A")} buttonText="Reserve a Slot" />
-      <RoomBar roomName="Gokongwei Room 307B" onReserve={() => handleReserve("307B")} buttonText="Reserve a Slot"/>
-      <RoomBar roomName="Gokongwei Room 404A" onReserve={() => handleReserve("404A")} buttonText="Reserve a Slot"/>
+      <RoomBar roomName="Gokongwei Room 307A" onReserve={() => handleReserve("Gokongwei 307A")} buttonText="Reserve a Slot" />
+      <RoomBar roomName="Gokongwei Room 307B" onReserve={() => handleReserve("Gokongwei 307B")} buttonText="Reserve a Slot"/>
+      <RoomBar roomName="Gokongwei Room 404A" onReserve={() => handleReserve("Gokongwei 404A")} buttonText="Reserve a Slot"/>
     </div>
   );
 };
