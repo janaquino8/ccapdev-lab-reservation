3cc73eb270e881cfd4977219c20e3fb3edb63eb6
Childax <richmondramos117@gmail.com>
2026-04-05 23:17:34 +0800

    fix: changed catchall route

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,7 @@ app.use('/users', userRouter);
 
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
